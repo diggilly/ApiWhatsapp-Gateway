d395814dab79fe988fe033a9191e4e9f357adbb2
kinflux digital <126244085+diggilly@users.noreply.github.com>
2024-09-20 08:11:20 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 // ============= GLOBAL SETTING ============ //
 global.port = "5001"; // Port Api / Browser
 global.countrycode = "KE"; // Country Code - https://countrycode.org/ (ISO CODES)
-global.countrycodephone = "62"; // Country Phone - https://countrycode.org/ (COUNTRY CODE)
+global.countrycodephone = "254"; // Country Phone - https://countrycode.org/ (COUNTRY CODE)
 global.timezone = "Asia/Jakarta"; // Time Zone
 global.usePairingNumber = true; // true = Pairing Code / false = QRCode
 global.pairingNumber = "254751599663"; // whatsapp number used as a bot, for pairing number
@@ -15,7 +15,7 @@ global.use_pp = true; // use a logo?
 //========================================================
 
 global.kontakadmin = ["254751599663"]; // admin whatsapp number
-global.kirimkontak_admin = false; // true = automatically send admin contact
+global.kirimkontak_admin = true; // true = automatically send admin contact
 
 //========================================================
 
